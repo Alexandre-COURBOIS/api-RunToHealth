cc3ef8bcb768b1dd0555a4371bc452a4825b00b5
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-06-29 12:33:16 +0200

    Update on rout name

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/api/user", name="user_")
+ * @Route("/api", name="user_")
  * Class RegisterController
  * @package App\Controller
  */
